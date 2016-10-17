9a642efeb934ee38cf197f0cef2d51436f94f929
Iulian <julian.stana@gmail.com>
2016-10-17 23:35:36 +0300

    [file-handler] save info logging to file

--- app/__init__.py
+++ app/__init__.py
@@ -34,5 +34,17 @@ if not app.debug:
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
+# file handling handler
+log_format = '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a',
+                                       1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter(log_format))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
 
 from app import views, models

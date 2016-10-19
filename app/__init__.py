4417085671975e490511bcc000bf7f0f2edac96e
Iulian <julian.stana@gmail.com>
2016-10-20 00:17:42 +0300

    [file-handler] refactoring code. Change smtp port to 2525

--- app/__init__.py
+++ app/__init__.py
@@ -24,12 +24,11 @@ if not app.debug:
     import logging
     from logging.handlers import SMTPHandler
     credentials = None
-    if MAIL_PASSWORD and MAIL_USERNAME:
+    if MAIL_USERNAME or MAIL_PASSWORD:
         credentials = (MAIL_USERNAME, MAIL_PASSWORD)
     mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT),
-                               'no-replay@' + MAIL_SERVER,
-                               ADMINS,
-                               'micro blog failure',
+                               'no-reply@' + MAIL_SERVER, ADMINS,
+                               'microblog failure',
                                credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
@@ -41,10 +40,11 @@ if not app.debug:
     from logging.handlers import RotatingFileHandler
     file_handler = RotatingFileHandler('tmp/microblog.log', 'a',
                                        1 * 1024 * 1024, 10)
-    file_handler.setFormatter(logging.Formatter(log_format))
-    app.logger.setLevel(logging.INFO)
     file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter(log_format))
+
     app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
     app.logger.info('microblog startup')
 
 from app import views, models
--- config.py
+++ config.py
@@ -19,7 +19,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # mail server settings
 MAIL_SERVER = 'localhost'
-MAIL_PORT = 25
+MAIL_PORT = 2525
 MAIL_USERNAME = None
 MAIL_PASSWORD = None
 

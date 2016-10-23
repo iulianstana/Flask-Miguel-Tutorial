1a1a4ea89162b6d7cd5de4e0152c0aee52727354
Iulian <julian.stana@gmail.com>
2016-10-23 23:13:56 +0300

    [posts] create a form for posts and add it to owr index page

--- app/views.py
+++ app/views.py
@@ -11,8 +11,8 @@ from app import lm
 from app import db
 from app import oid
 
-from .models import User
-from .forms import LoginForm, EditForm
+from .models import User, Post
+from .forms import LoginForm, EditForm, PostForm
 
 
 @app.errorhandler(404)
@@ -35,13 +35,18 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     # use login user credentials
-    user = g.user
-
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, timestamp=datetime.utcnow(), author=g.user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'nickname': 'Nuca'},
@@ -54,8 +59,9 @@ def index():
     ]
 
     return render_template('index.html',
-                            user=user,
-                            posts=posts)
+                           user=user,
+                           form=form,
+                           posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])

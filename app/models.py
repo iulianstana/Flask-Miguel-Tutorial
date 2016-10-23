668147c29b3bed498f70f2ede5145ba3051aee10
Iulian <julian.stana@gmail.com>
2016-10-23 19:36:51 +0300

    [followers] method that extract followers posts

--- app/models.py
+++ app/models.py
@@ -69,6 +69,12 @@ class User(db.Model):
             self.followed.remove(user)
             return self
 
+    def followed_posts(self):
+        return Post.query.join(followers,
+                               (followers.c.followed_id == Post.user_id))\
+                         .filter(followers.c.follower_id == self.id)\
+                         .order_by(Post.timestamp.desc())
+
     def __repr__(self):
         return '<User %r>' % self.nickname
 

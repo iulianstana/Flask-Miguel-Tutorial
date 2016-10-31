b52300f927d02b7d310decb33e85d017e6a4bf21
Iulian <julian.stana@gmail.com>
2016-10-31 23:57:25 +0200

    [mail] follower notification using flask_mail
    Create a text email template and a html template in order to send
    nicer emails.

--- app/emails.py
+++ app/emails.py
@@ -1,4 +1,8 @@
+from flask import render_template
 from flask_mail import Message
+
+from config import ADMINS
+
 from app import mail
 
 
@@ -7,3 +11,14 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
+
+
+def follower_notification(followed, follower):
+    send_email("[microblog] %s is now following you!" % follower.nickname,
+               ADMINS[0],
+               [followed.email],
+               render_template("follower_email.txt",
+                               user=followed, follower=follower),
+               render_template("follower_email.html",
+                               user=followed, follower=follower)
+               )

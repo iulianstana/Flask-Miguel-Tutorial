7e6692ea8a3127971be25b1975d074e76cb4b323
Iulian <julian.stana@gmail.com>
2016-11-04 00:21:37 +0200

    [mail] send email asynchronous using Threading

--- app/emails.py
+++ app/emails.py
@@ -1,16 +1,25 @@
+from threading import Thread
+
 from flask import render_template
 from flask_mail import Message
 
 from config import ADMINS
 
 from app import mail
+from app import app
+
+
+def send_async_email(application, msg):
+    with application.app_context():
+        mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
 
 
 def follower_notification(followed, follower):
